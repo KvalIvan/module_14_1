100ce273f5e51c8fe08dc384b5b08c4207e6f002
KvalIvan <serga.vanya14@gmail.com>
2024-11-29 10:43:33 +0300

    module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -27,7 +27,7 @@ for j in range(1, 11, 3):
 cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
 users = cursor.fetchall()
 for user in users:
-    print(f"Имя:{user[0]} | Почта:{user[1]} | Возраст:{user[2]} | Баланс:{user[3]}")
+    print(f'Имя:{user[0]} | Почта:{user[1]} | Возраст:{user[2]} | Баланс:{user[3]}')
 
 connection.commit()
 connection.close()
